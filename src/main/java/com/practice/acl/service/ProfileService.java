cd99334281960176a28ab411d3aa549790899a87
Navneet Prabhakar <navjagat@gmail.com>
2015-06-06 16:33:27 +0530

    permissions added

--- src/main/java/com/practice/acl/service/ProfileService.java
+++ src/main/java/com/practice/acl/service/ProfileService.java
@@ -8,14 +8,18 @@ import com.practice.acl.entiry.Profile;
 
 public interface ProfileService {
 	
-	@PreAuthorize("hasPermission(#profile,'read') and #oauth2.hasScope('read')")
+	@PreAuthorize("hasPermission(#profile,'read') and #oauth2.hasScope('write')")
 	public Profile create(Profile profile);
 	
+	@PreAuthorize("hasPermission(#id,'read') and #oauth2.hasScope('trust')")
 	public void delete(Long id);
 	
+	@PreAuthorize("hasPermission(#profile,'read') and #oauth2.hasScope('write')")
 	public Profile update(Profile profile);
 	
+	@PreAuthorize("hasPermission(#id,'read') and #oauth2.hasScope('read')")
 	public Profile retriece(Long id);
 	
+	@PreAuthorize("hasPermission(#profile,'read') and #oauth2.hasScope('read')")
 	public List<Profile> retrieveAll();
 }
