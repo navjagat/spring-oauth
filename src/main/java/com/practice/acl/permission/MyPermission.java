ea54e300531786c269d9ced174320edcc180aebd
Navneet Prabhakar <navjagat@gmail.com>
2015-06-06 16:45:24 +0530

    permissions refined

--- src/main/java/com/practice/acl/permission/MyPermission.java
+++ src/main/java/com/practice/acl/permission/MyPermission.java
@@ -2,25 +2,41 @@ package com.practice.acl.permission;
 
 import java.io.Serializable;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.PermissionEvaluator;
 import org.springframework.security.core.Authentication;
 
 import com.practice.acl.entiry.Profile;
+import com.practice.acl.repo.ClientRepository;
+import com.practice.acl.entiry.Client;
 
 public class MyPermission implements PermissionEvaluator {
-
+	
+	@Autowired
+	private ProfileRepository repo;
+	
+	@Autowired
+	private ClientRepository repository;
+	
 	@Override
 	public boolean hasPermission(Authentication authentication,
 			Object targetDomainObject, Object permission) {
 		
+		Profile profile = null;
+		
 		if(targetDomainObject==null)
 			return true;
-				
-		Profile profile = (Profile) targetDomainObject;
+			
+		if(targetDomainObject instance of Long){
+			profile = repo.findOne((Long) targetDomainObject);
+		}
+		if(targetDomainObject instance of Profile){
+			profile = (Profile) targetDomainObject;
+		}
 		
-		String clientId = (String) authentication.getPrincipal();
+		Client client = repository.findOne((String) authentication.getPrincipal());
 		
-		return clientId.equals(profile.getCompany());
+		return client.getAccount().equals(profile.getCompany());
 	}
 
 	@Override
